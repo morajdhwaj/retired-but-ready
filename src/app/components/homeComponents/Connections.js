768e559284a391f2822cb6a9a3dbeded92d262ed
Moraj <109418239+morajdhwaj@users.noreply.github.com>
2023-12-27 13:45:37 +0530

    [ADD] Video components

--- src/app/components/homeComponents/Connections.js
+++ src/app/components/homeComponents/Connections.js
@@ -1,22 +1,35 @@
 import Image from "next/image";
 import React from "react";
+import { FaLayerGroup } from "react-icons/fa6";
 
 const Connections = () => {
   return (
-    <div className="pl-20 bg-gray-100 flex justify-end pt-20">
-      <div className="flex flex-col gap-5 items-center justify-center ">
-        <h1 className="font-semibold text-5xl text-center ">
-          The world's only exclusive platform and
-        </h1>
-        <h1 className="font-semibold text-5xl text-center">
-          community for retired professional
-        </h1>
+    <div>
+      <div className="relative ">
+        <div className=" absolute w-4/5 h-[63vh] bg-transparent  flex items-end justify-end">
+          <div className=" text-center bg-white px-4 py-2 rounded-lg flex items-center justify-center ">
+            <div className="bg-[#F9AE34] p-1 rounded-full">
+              <FaLayerGroup size={20} color="white" />
+            </div>
+            <div className="ml-2">
+              <h1 className="font-semibold">100,000+</h1>
+              <p className="text-xs">connections</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex justify-end items-end ">
-        <Image src="/assets/13.png" height={400} width={400} />
-      </div>
-      <div className="">
-        <div className="bg-white">100,000+</div>
+      <div className="pl-20 bg-gray-100 flex justify-end pt-20">
+        <div className="flex flex-col gap-5 items-center justify-center ">
+          <h1 className="font-semibold text-5xl text-center ">
+            The world's only exclusive platform and
+          </h1>
+          <h1 className="font-semibold text-5xl text-center">
+            community for retired professional
+          </h1>
+        </div>
+        <div className="flex justify-end items-end ">
+          <Image src="/assets/13.png" height={400} width={400} />
+        </div>
       </div>
     </div>
   );
---
+++ src/app/components/homeComponents/VideoComponent.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const VideoComponent = () => {
+  return (
+    <div className="bg-[#EDEBF2] px-20 flex py-40  ">
+      <div className="w-1/2 flex flex-col gap-5">
+        <h1 className="font-semibold text-5xl leading-[65px] ">
+          Where young minds meet industrious retires
+        </h1>
+        <p className="text-gray-500 text-xl font-light leading-8">
+          Retired professional have a wealth of knowledge, wisdom and skill that
+          can be beneficial to current employed professional.RBR is where this
+          juncture is made possible.
+        </p>
+        <button className="bg-[#f77d53] p-3 text-white self-start font-semibold rounded-xl">
+          Start your journey
+        </button>
+      </div>
+      <div className="w-1/2 flex items-center justify-center ">
+        <div className="bg-[#cab7e4] p-2 rounded-lg">
+          <video height={400} width={500} className="rounded-lg" controls>
+            <source src="/assets/RBRV4WhatsApp.mp4" type="video/mp4"></source>
+          </video>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoComponent;
