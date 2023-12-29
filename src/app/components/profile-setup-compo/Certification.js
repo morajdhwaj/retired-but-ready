400498187a3f37ae51dd1873317b0972264084bb
shivdeep2002 <iamshivdeep@gmail.com>
2023-12-29 15:50:18 +0530

    Update code

--- src/app/components/profile-setup-compo/Certification.js
+++ src/app/components/profile-setup-compo/Certification.js
@@ -3,39 +3,39 @@ import React from "react";
 const Certification = () => {
   return (
     <div>
-      <div className=" flex flex-col mx-40">
+      <div className=" flex flex-col mx-20">
         <h1 className="text-2xl font-bold ">Certifications</h1>
-        <h3 className="text-gray-400 text-noraml mt-5  ">
+        <h3 className="text-gray-500 text-noraml mt-5  ">
           [Example:Microsoft Certifified Solution Associate (MCSA)]
         </h3>
         <div className="mt-8 flex flex-col gap-4 w-auto  ">
           <input
             type="text"
-            className="bg-gray-100 border rounded-lg   border-gray-300 py-1"
+            className="bg-[#f2f1f3] border border-gray-300 h-10   rounded w-full"
           />
           <input
             type="text"
-            className="bg-gray-100 border  border-gray-300 py-1  rounded-lg  "
+            className="bg-[#f2f1f3] border border-gray-300 h-10   rounded w-full"
           />
           <input
             type="text"
-            className="bg-gray-100 border  border-gray-300 py-1  rounded-lg "
+            className="bg-[#f2f1f3] border border-gray-300 h-10   rounded w-full"
           />
           <input
             type="text"
-            className="bg-gray-100 border   border-gray-300 py-1  rounded-lg  "
+            className="bg-[#f2f1f3] border border-gray-300 h-10   rounded w-full"
           />
           <input
             type="text"
-            className="bg-gray-100 border  border-gray-300 py-1  rounded-lg  "
+            className="bg-[#f2f1f3] border border-gray-300 h-10   rounded w-full"
           />
           <input
             type="text"
-            className="bg-gray-100 border  border-gray-300 py-1  rounded-lg  "
+            className="bg-[#f2f1f3] border border-gray-300 h-10   rounded w-full"
           />
         </div>
-        <div className="mt-8  font-semibold">
-          <h2 className="text-gray-700  text-lg">
+        <div className="mt-8">
+          <h2 className="text-gray-500  text-lg">
             Would you like to charge for your services?
           </h2>
           <div className="flex  items-center gap-5 mt-3 ">
@@ -46,10 +46,10 @@ const Certification = () => {
           </div>
 
           <div className=" mt-5 ">
-            <h1 className="text-gray-700  text-lg">Currency you accept</h1>
+            <h1 className="text-gray-500  text-base">Currency you accept</h1>
             <select
               type="text"
-              className="bg-gray-100 border rounded-lg w-[500px]  border-gray-300 py-1 mt-3"
+              className="bg-[#f2f1f3] border border-gray-300 h-10   rounded w-full"
             >
               <option>Option 1</option>
               <option>Option 2</option>
@@ -58,12 +58,12 @@ const Certification = () => {
               <option> Option 5</option>
             </select>
           </div>
-          <div className="text-gray-700  text-xl mt-5">
+          <div className="text-gray-500  text-xl mt-5">
             <h1>About you</h1>
           </div>
         </div>
         <div>
-          <h3 className="text-gray-700 text-lg  mt-3 w-[100%]">
+          <h3 className="text-gray-500 text-sm  mt-3 w-[100%]">
             Keep it crisp.Keep is classy.You can write about your years of
             experience,Industry or achievements.You can also share your hobbies
             Or Quirks
@@ -75,7 +75,7 @@ const Certification = () => {
           "
           className="bg-gray-100 border rounded-lg mt-3   border-gray-300    h-[200px]"
         />
-        <h2 className="text-gray-700  text-lg mt-10">
+        <h2 className="text-gray-500  text-sm mt-10">
           Max character limit-1000
         </h2>
 
@@ -84,10 +84,11 @@ const Certification = () => {
             Preview as visitor
           </h1>
           <div className="text-center mt-5 flex gap-10 items-center justify-center rounded ">
-            <button className="border-2 border-[#773FC6] w-[200px] text-[#773FC6] py-1 text-xl font-semibold ">
+            {" "}
+            <button className="text-[#773fc6] border-2 border-[#773fc6]  w-[200px]  font-medium  rounded py-1">
               Go back
             </button>
-            <button className="border-2 border-[#773FC6] w-[200px]  py-1 text-xl font-semibold  bg-[#773FC6] text-white rounded">
+            <button className="bg-[#773fc6]  w-[200px] text-white font-medium rounded py-1 ">
               Next
             </button>
           </div>
