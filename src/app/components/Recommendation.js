37c572cd615c7ddfd7971fea8a18774ff98b35c1
shivdeep2002 <iamshivdeep@gmail.com>
2024-02-28 19:13:30 +0530

    responsive join page

--- src/app/components/Recommendation.js
+++ src/app/components/Recommendation.js
@@ -4,72 +4,65 @@ import Image from "next/image";
 import { BsThreeDotsVertical } from "react-icons/bs";
 
 const Recommendation = () => {
+  const Data = [
+    {
+      information: "Lorem ipsum dolor sit glitch!",
+      member: "201 Members",
+      paragraph: "Lorem ipsum dolor sit glitch!",
+    },
+    {
+      information: "Lorem ipsum dolor sit glitch!",
+      member: "202 Members",
+      paragraph: "Lorem ipsum dolor sit glitch!",
+    },
+    {
+      information: "Lorem ipsum dolor sit glitch!",
+      member: "203 Members",
+      paragraph: "Lorem ipsum dolor sit glitch!",
+    },
+    {
+      information: "Lorem ipsum dolor sit glitch!",
+      member: "204 Members",
+      paragraph: "Lorem ipsum dolor sit glitch!",
+    },
+    {
+      information: "Lorem ipsum dolor sit glitch!",
+      member: "205 Members",
+      paragraph: "Lorem ipsum dolor sit glitch!",
+    },
+  ];
   return (
     <div>
-      <div className="flex ">
-        <div className="  ">
-          <div className="flex justify-between   border-gray-200 w-full">
-            <div className="flex mt-2">
-              <Image
-                src="/assets/Ellipse-39.png"
-                width={40}
-                height={40}
-                alt="pic"
-                className=" w-24  h-24 rounded-full border-2   border-gray-200 "
-              />
-              <div className="mt-5 mx-2">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                <p>245 Member</p>
+      {Data.map((curelem, key) => (
+        <div>
+          <div className="mt-5 flex justify-between" key={key}>
+            <div className="flex">
+              <div className="mt-3">
+                <Image
+                  src="/assets/Ellipse-39.png"
+                  width={50}
+                  height={50}
+                  alt="pic"
+                  className="w-16 h-16 rounded-full border-2 border-gray-200"
+                />
+              </div>
+              <div className="mx-2 mt-1">
+                <p className="text-base font-medium lg:text-lg lg:font-semibold">
+                  {curelem.information}
+                </p>
+                <p className=" text-sm md:text-sm ">{curelem.member}</p>
+                <p className=" text-sm md:text-sm ">{curelem.paragraph}</p>
               </div>
             </div>
-            <div className="flex mx-5 items-center">
-              <button>
-                <BsThreeDotsVertical />
+            <div>
+              <button className="border-2 border-[#A8359C] p-1 w-14 lg:w-20 text-center font-bold rounded-lg mt-5 mx-2 lg:mx-0">
+                Join
               </button>
             </div>
           </div>
-          <div className="flex justify-between  border-b-2 border-gray-200 w-full">
-            <div className="flex mt-2">
-              <Image
-                src="/assets/Ellipse-39.png"
-                width={40}
-                height={40}
-                alt="pic"
-                className=" w-24  h-24 rounded-full border-2   border-gray-200 "
-              />
-              <div className="mt-5 mx-2">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                <p>245 Member</p>
-              </div>
-            </div>
-            <div className="flex mx-5 items-center">
-              <button>
-                <BsThreeDotsVertical />
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between  border-b-2 border-gray-200 w-full">
-            <div className="flex mt-2">
-              <Image
-                src="/assets/Ellipse-39.png"
-                width={40}
-                height={40}
-                alt="pic"
-                className=" w-24  h-24 rounded-full border-2   border-gray-200 "
-              />
-              <div className="mt-5 mx-2">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                <p>245 Member</p>
-              </div>
-            </div>
-            <div className="flex mx-5 items-center">
-              <button>
-                <BsThreeDotsVertical />
-              </button>
-            </div>
-          </div>
+          <div className="border border-gray-200 mt-2 mx-2" />
         </div>
-      </div>
+      ))}
     </div>
   );
 };
