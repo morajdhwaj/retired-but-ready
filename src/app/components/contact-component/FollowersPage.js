7a09bc74a266f7674054d1a3cd5677a91cffbbfe
JaishreeNishad <Jaysh2222@gmail.com>
2024-02-29 12:14:56 +0530

    API integration in conatct/FollowersPgae

--- src/app/components/contact-component/FollowersPage.js
+++ src/app/components/contact-component/FollowersPage.js
@@ -4,10 +4,14 @@ import Image from "next/image";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { PiShareFatLight } from "react-icons/pi";
 import axios from "axios";
+import PopUp from "@/app/components/PopUp";
 
 const FollowersPage = () => {
-  const [follower, setFollowers] = useState([]);
+  const [followers, setFollowers] = useState([]);
   const [userId, setUserId] = useState("");
+  const [show, setShow] = useState({});
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [selectedFollower, setSelectedFollower] = useState(null);
 
   useEffect(() => {
     setUserId(localStorage.getItem("userId"));
@@ -31,45 +35,51 @@ const FollowersPage = () => {
       });
   };
 
-  console.log(follower, "follower ka data");
+  const handleShow = (index) => {
+    setShow((prevShow) => ({
+      ...prevShow,
+      [index]: !prevShow[index],
+    }));
+  };
 
-  // const contacts = [
-  //   {
-  //     name: "Jaya",
-  //     role: "OPPO India Marketing Head",
-  //     company: "Tech Connection India Pvt.Ltd.",
-  //     lastConnected: "Connected 2 days ago",
-  //   },
-  //   {
-  //     name: "Parul",
-  //     role: "Role for Parul",
-  //     company: "Company for Parul",
-  //     lastConnected: "Connected 2 days ago",
-  //   },
-  //   {
-  //     name: "Kimjan",
-  //     role: "Role for Kimjan",
-  //     company: "Company for Kimjan",
-  //     lastConnected: "Connected 2 days ago",
-  //   },
-  // ];
+  const handleDeleteModal = (follower) => {
+    setSelectedFollower(follower);
+    setShowDeleteModal(true);
+  };
+
+  const deleteFollowers = () => {
+    const options = {
+      method: "DELETE",
+      url: `https://retpro.catax.me/remove-follower/${userId}/${selectedFollower.from_user_id}`,
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        getFollowers();
+        setShowDeleteModal(false);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   return (
     <div>
-      {follower.length > 0 &&
-        follower.map((followItem, index) => (
+      {followers.length > 0 &&
+        followers.map((follower, index) => (
           <div className="flex mt-5" key={index}>
             <div className="w-3/4  flex justify-between items-center">
               <div className="flex">
                 <div className="">
                   <Image
-                    // src="/assets/Ellipse-39.png"
                     width={50}
                     height={50}
                     alt="pic"
                     src={
-                      followItem.from_user_image
-                        ? followItem.from_user_image
+                      follower.from_user_image
+                        ? follower.from_user_image
                         : "/assets/Ellipse-39.png"
                     }
                     className="rounded "
@@ -77,7 +87,7 @@ const FollowersPage = () => {
                 </div>
                 <div className="">
                   <h1 className="text-[#2C2C2C] font-medium">
-                    {followItem.from_user_full_name}
+                    {follower.from_user_full_name}
                   </h1>
                   <p className="text-[#888888] font-medium text-sm">
                     Human resource executive
@@ -95,8 +105,20 @@ const FollowersPage = () => {
             <div className="w-1/4  flex items-center justify-center">
               <div>
                 <button className="text-2xl text-gray-500">
-                  <BsThreeDotsVertical />
+                  <BsThreeDotsVertical onClick={() => handleShow(index)} />
                 </button>
+                {show[index] && (
+                  <div className="absolute border bg-white border-gray-300 shadow-md rounded-md flex flex-col right-48 px-2 ">
+                    <div className="flex flex-col p-2 items-center justify-center">
+                      <button
+                        onClick={() => handleDeleteModal(follower)}
+                        className="hover:bg-[#773fc6] w-20 rounded-md hover:text-white text-black p-2"
+                      >
+                        Unfollow
+                      </button>
+                    </div>
+                  </div>
+                )}
                 <button className="text-2xl text-gray-500">
                   <PiShareFatLight />
                 </button>
@@ -104,6 +126,17 @@ const FollowersPage = () => {
             </div>
           </div>
         ))}
+
+      {showDeleteModal && (
+        <PopUp
+          close={handleDeleteModal}
+          onClick={deleteFollowers}
+          title="Are you want Remove this follower"
+          action="Delete"
+          message=""
+          error="error"
+        />
+      )}
     </div>
   );
 };
