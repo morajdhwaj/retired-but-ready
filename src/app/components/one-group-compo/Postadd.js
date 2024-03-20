c9bb17b78f54be96b922e838387f56170798afe3
JaishreeNishad <Jaysh2222@gmail.com>
2024-03-20 11:31:27 +0530

    API integrate in Group inside page

--- src/app/components/one-group-compo/Postadd.js
+++ src/app/components/one-group-compo/Postadd.js
@@ -1,26 +1,56 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { FaEllipsisVertical } from "react-icons/fa6";
 import Image from "next/image";
 import { MdOutlineComment } from "react-icons/md";
 import { AiOutlineLike } from "react-icons/ai";
-const Postadd = () => {
-  const element = [
-    {
-      name: "AMAN",
-    },
-    {
-      name: "MORAJ",
-    },
-    {
-      name: "YASEEN",
-    },
-  ];
+import { UserIdContext } from "@/context/UserIdContext";
+import axios from "axios";
+
+const Postadd = ({ groupId }) => {
+  const [groupPost, setGroupPost] = useState([]);
+  const [userId, setUserId] = useState("");
+  const { userIdFromContext } = useContext(UserIdContext);
+
+  useEffect(() => {
+    setUserId(userIdFromContext);
+    getAllGroupPost();
+  }, [userId]);
+
+  const getAllGroupPost = () => {
+    const options = {
+      method: "GET",
+      url: `https://retpro.catax.me/get-all-group-posts/${groupId}`,
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setGroupPost(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  console.log(groupId, "POSTADDD KA groupId");
+  // const element = [
+  //   {
+  //     name: "AMAN",
+  //   },
+  //   {
+  //     name: "MORAJ",
+  //   },
+  //   {
+  //     name: "YASEEN",
+  //   },
+  // ];
 
   return (
     <>
-      {element.map((groupElem, index) => (
+      {groupPost.map((groupElem, index) => (
         <div
           className=" w-3/4 mt-5 rounded-md border border-gray-400 p-5"
           key={index}
@@ -34,8 +64,12 @@ const Postadd = () => {
                 alt="pic"
               />
               <div>
-                <h1>{groupElem.name}</h1>
-                <p className="text-xs text-gray-400">Software Developer</p>
+                <h1 className="text-sm">
+                  {groupElem.post_user.user_display_name}
+                </h1>
+                <p className="text-xs text-gray-400">
+                  {groupElem.post_user.last_designation}
+                </p>
                 <p className="text-xs text-gray-400">Admin</p>
               </div>
             </div>
@@ -45,13 +79,9 @@ const Postadd = () => {
           </div>
           <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-400"></hr>
           <div>
-            <h1>
-              Yaseen group name private Unlisted Start a post in this group
-              Media Text Yaseen Software Developer Admin Like Comment Yaseen
-              Software.
-            </h1>
+            <h1>{groupElem.post_description}</h1>
             <div className="flex items-center justify-center">
-              <Image src="/assets/13.jpg" width={400} height={650} alt="pic" />
+              {/* <Image src="/assets/13.jpg" width={400} height={650} alt="pic" /> */}
             </div>
             <div className="flex justify-between items-center mt-2">
               <div className="flex items-center justify-center gap-2">
