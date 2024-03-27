21fed1227323620142f54d478951d2a6e1488ca4
aman <amanpatel22012003@gmail.com>
2024-03-27 16:44:39 +0530

    commit

--- src/app/(pages)/connections/[userId]/page.js
+++ src/app/(pages)/connections/[userId]/page.js
@@ -9,10 +9,12 @@ import axios from "axios";
 import { FaUserCircle } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
+import dayjs from "dayjs";
 
 const page = ({ params }) => {
   const profileId = params["userId"];
   const [connections, setConnections] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     getAllConnection();
@@ -30,6 +32,14 @@ const page = ({ params }) => {
     }
   };
 
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredConnections = connections.filter((data) =>
+    data.user_full_name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   console.log(connections, profileId, "this is get my all connections ");
 
   return (
@@ -44,19 +54,21 @@ const page = ({ params }) => {
           </div>
           <div className="bg-white lg:mt-[100px] max-w-[1000px] lg:mx-5  lg:ml-[260px]  m-auto rounded-lg px-5 md:px-20 py-10 mt-5 sm:mt-0">
             <div className="">
-              <h1 className="ml-1 text-lg font-semibold">
-                {connections.length} Connections
-              </h1>
               <div className="flex justify-between border-b-2">
-                <select name="" id="" className="text-md">
+                {/* <select name="" id="" className="text-md">
                   <option value="">Recently Added</option>
                   <option value="">First Name</option>
                   <option value="">Last Name</option>
-                </select>
+                </select> */}
+                <h1 className="ml-1 text-lg font-semibold flex items-center">
+                  {connections.length} Connections
+                </h1>
 
                 <div className="p-2 relative">
                   <input
                     type="search"
+                    value={searchQuery}
+                    onChange={handleSearchChange}
                     className="w-full bg-[#E4E7EB border-2 pl-9 pr-3 py-2 rounded-lg outline-none "
                     placeholder="Search messages"
                   />
@@ -66,36 +78,41 @@ const page = ({ params }) => {
                 </div>
               </div>
               <div className="">
-                {connections.map((data) => (
+                {filteredConnections.map((data) => (
                   <div
                     className="py-2 px-2 sm:flex justify-between items-center border-b "
                     key={data?._id}
                   >
-                    {data?.from_user === profileId ? (
-                      <Link
-                        href={`/profile/${data?.to_user}`}
-                        className="flex items-center gap-3"
-                      >
-                        {data?.to_user_image ? (
-                          <Image
-                            alt="rtr-pic"
-                            src={data?.to_user_image}
-                            height={70}
-                            width={70}
-                            className="w-20 h-20 rounded-full border-2 border-gray-200"
-                          />
-                        ) : (
-                          <FaUserCircle color="gray" size={70} />
-                        )}
-                        <div className="">
-                          <h2 className="text-md font-semibold">
-                            {data?.to_user_full_name}
-                          </h2>
-                          <h3 className="font-medium">Developer</h3>
-                          <p className="text-sm">Connected 3 hours ago</p>
-                        </div>
-                      </Link>
-                    ) : (
+                    {/* {data?.from_user === profileId ? ( */}
+                    <Link
+                      href={`/profile/${data?.user_id}`}
+                      className="flex items-center gap-3"
+                    >
+                      {data?.user_image ? (
+                        <Image
+                          alt="rtr-pic"
+                          src={data?.user_image}
+                          height={70}
+                          width={70}
+                          className="w-20 h-20 rounded-full border-2 border-gray-200"
+                        />
+                      ) : (
+                        <FaUserCircle color="gray" size={70} />
+                      )}
+                      <div className="">
+                        <h2 className="text-md font-semibold">
+                          {data?.user_full_name}
+                        </h2>
+                        {/* <h3 className="font-medium">Developer</h3> */}
+                        <p className="text-sm">
+                          Connected{" "}
+                          {dayjs(data?.network_sent_on).format(
+                            "YYYY-MM-DD HH:mm"
+                          )}
+                        </p>
+                      </div>
+                    </Link>
+                    {/* ) : (
                       <Link
                         href={`/profile/${data?.from_user}`}
                         className="flex items-center gap-3"
@@ -119,7 +136,7 @@ const page = ({ params }) => {
                           <p className="text-sm">Connected 3 hours ago</p>
                         </div>
                       </Link>
-                    )}
+                    )} */}
 
                     {/* <div className="flex gap-3 mt-3 sm:mt-0 justify-center pl-5">
                       <button className=" py-1 px-7 border-2 rounded-lg border-[#773FC6] h-10">
